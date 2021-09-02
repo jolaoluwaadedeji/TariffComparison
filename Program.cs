2f6ade405f5876b74a5ee55420d54cc4bd612ac1
jolaoluwaadedeji <adedejijolaoluwa@gmail.com>
2021-09-02 04:55:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TariffComparison.Domain;
+using TariffComparison.Domain.Services;
+
+namespace TariffComparison
+{
+    class Program
+    {
+        private static readonly ITariffComparer _tariffComparer = new TariffComparer();
+        static void Main(string[] args)
+        {
+            var result = _tariffComparer.CompareCost(new List<decimal>() {3500.00M, 4500.00M, 6000.00M });
+        }
+    }
+}
